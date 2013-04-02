f8aa6a4cf5d55e2d3d6a65a213648d8ffea2b7b1
Julian Ceipek <julian.ceipek@gmail.com>
2013-04-02 14:18:52 -0400

    Added visualization for AI Navmesh geometry.

--- UnityPathing/Assets/Pathfinding Scripts/NavmeshGeometry.js
+++ UnityPathing/Assets/Pathfinding Scripts/NavmeshGeometry.js
@@ -1,3 +1,14 @@
 #pragma strict
 
 var verts : Vector3[];
+
+function OnDrawGizmos() {
+	Gizmos.color = Color.blue;
+	var networkTransform : Transform = transform.parent;
+	
+	for (var i = 0; i < verts.length; i++) {
+		var offset = networkTransform.position;
+        Gizmos.DrawLine (offset+networkTransform.TransformDirection(verts[i]), 
+        	             offset+networkTransform.TransformDirection(verts[(i+1)%verts.length]));
+	}
+}
