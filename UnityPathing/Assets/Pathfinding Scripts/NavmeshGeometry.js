7b751be0593edfd95c49fb0ab9ffdc3d8ae90637
Julian Ceipek <julian.ceipek@gmail.com>
2013-04-02 14:32:30 -0400

    Transformed verts are only cached at runtime.

--- UnityPathing/Assets/Pathfinding Scripts/NavmeshGeometry.js
+++ UnityPathing/Assets/Pathfinding Scripts/NavmeshGeometry.js
@@ -3,8 +3,11 @@
 var verts : Vector3[];
 private var transformedVerts : Vector3[] = null;
 
+// Cache transformed vertex positions if game is running
+// This must be modified if we expect the navmesh to move 
+// while the game is running.
 function getTransformedVerts() : Vector3[] {
-	if (transformedVerts == null) {
+	if (!Application.isPlaying || transformedVerts == null) {
 		var networkTransform : Transform = transform.parent;
 		transformedVerts = new Vector3[3];
 		for (var i = 0; i < verts.length; i++) {
