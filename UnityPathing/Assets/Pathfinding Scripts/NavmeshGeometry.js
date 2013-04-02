5e224dbb05a6d2c3db06c5beed3d8b7d65e13ef9
Julian Ceipek <julian.ceipek@gmail.com>
2013-04-02 14:24:52 -0400

    Added helper to get transformed verts.

--- UnityPathing/Assets/Pathfinding Scripts/NavmeshGeometry.js
+++ UnityPathing/Assets/Pathfinding Scripts/NavmeshGeometry.js
@@ -1,14 +1,23 @@
 #pragma strict
 
 var verts : Vector3[];
+private var transformedVerts : Vector3[] = null;
+
+function getTransformedVerts() : Vector3[] {
+	if (transformedVerts == null) {
+		var networkTransform : Transform = transform.parent;
+		transformedVerts = new Vector3[3];
+		for (var i = 0; i < verts.length; i++) {
+			transformedVerts[i] = networkTransform.position + networkTransform.TransformDirection(verts[i]);
+		}
+	}
+	return transformedVerts;
+}
 
 function OnDrawGizmos() {
 	Gizmos.color = Color.blue;
-	var networkTransform : Transform = transform.parent;
-	
-	for (var i = 0; i < verts.length; i++) {
-		var offset = networkTransform.position;
-        Gizmos.DrawLine (offset+networkTransform.TransformDirection(verts[i]), 
-        	             offset+networkTransform.TransformDirection(verts[(i+1)%verts.length]));
+	var tVerts : Vector3[] = this.getTransformedVerts();
+	for (var i = 0; i < tVerts.length; i++) {
+        Gizmos.DrawLine (tVerts[i], tVerts[(i+1)%tVerts.length]);
 	}
 }
