4cceb25381e1801b3672c8257221cc42dcaaa194
Alex Adkins <arcellant@gmail.com>
2013-04-12 14:47:55 -0400

    Added edge finder.

---
+++ UnityPathing/Funneling.js
@@ -0,0 +1,18 @@
+#pragma strict
+
+// Copyright (C) 2013 Alex Adkins
+//
+// An implementation of funnelling. 
+//
+
+private var controller : CharacterController;
+
+// Use this for initialization
+function Start () {
+	controller = GetComponent(CharacterController);
+}
+
+// Update is called once per frame
+function Update () {
+}
+
