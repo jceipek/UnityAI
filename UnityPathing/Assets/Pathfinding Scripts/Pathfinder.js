8306258b4c535de817463d019b9b757d0e5fc334
Julian Ceipek <julian.ceipek@gmail.com>
2013-04-12 16:35:44 -0400

    Reorganization.

--- UnityPathing/Assets/Pathfinding Scripts/Pathfinder.js
+++ UnityPathing/Assets/Pathfinding Scripts/Pathfinder.js
@@ -15,6 +15,42 @@ var endWaypoint : Waypoint;
 @HideInInspector
 var idealWaypointPath : List.<Waypoint>; // Used to debug the path found during search
 
+//returns -1 when to the left, 1 to the right, and 0 for forward/backward
+//adapted from: http://forum.unity3d.com/threads/31420-Left-Right-test-function
+public static function AngleDir(fwd: Vector3, targetDir: Vector3, up: Vector3) : int {
+    var perp: Vector3 = Vector3.Cross(fwd, targetDir);
+    var dir: float = Vector3.Dot(perp, up);
+
+    if (dir > 0.0) {
+        return 1;
+    } else if (dir < 0.0) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+function GetClosestWaypointToPoint (point : Vector3) : Waypoint {
+    var minDist : float = Mathf.Infinity;
+    var closest : Waypoint;
+    
+    var waypoints : Component[];
+    waypoints = gameObject.GetComponentsInChildren (Waypoint);
+    for (var waypoint : Component in waypoints) {
+        var currDist = (waypoint.gameObject.transform.position - point).magnitude;
+        if (currDist < minDist) {
+            minDist = currDist;
+            closest = waypoint as Waypoint;
+        }
+    }        
+
+    return closest;
+}
+
+//-------------------------------------------------------------------
+// A* Functions
+//-------------------------------------------------------------------
+
 function HeuristicCostEstimate(from : Waypoint, to : Waypoint) : float {
 	return (from.gameObject.transform.position - to.gameObject.transform.position).magnitude;
 }
@@ -23,6 +59,7 @@ function WaypointCost(from : Waypoint, to : Waypoint) : float {
 	return (from.gameObject.transform.position - to.gameObject.transform.position).magnitude;
 }
 
+// Uses A* to find the optimal path between two points in a Waypoint network.
 function FindRoute (start : Waypoint, goal : Waypoint) : List.<Waypoint> {
 	var frontier : SortedList.<float, Waypoint> = new SortedList.<float, Waypoint>(); // Priority Queue, what have we not explored?
 	var explored : HashSet.<Waypoint> = new HashSet.<Waypoint>(); // Set, what have we explored?
@@ -43,14 +80,6 @@ function FindRoute (start : Waypoint, goal : Waypoint) : List.<Waypoint> {
 			// We found the solution! Reconstruct it.
 			var path : List.<Waypoint> = new List.<Waypoint>();
 			var pointer : Waypoint = goal;
-			/*for(var e=pathTo.GetEnumerator(); e.MoveNext();) {
-				if (e.Current.Value != null) {
-					Debug.Log(e.Current.Key.gameObject.name + " : " + e.Current.Value.gameObject.name);	
-				} else {
-					Debug.Log(e.Current.Key.gameObject.name + " : null");	
-				}
-				
-			}*/
 
 			while (pointer != null) {
 				path.Add(pointer);
@@ -65,11 +94,6 @@ function FindRoute (start : Waypoint, goal : Waypoint) : List.<Waypoint> {
 		for (var i = 0; i < leaf.linkedTo.length; i++) {
 			var connected : Waypoint = leaf.linkedTo[i];
 			if (!explored.Contains(connected) && !frontier.ContainsValue(connected)) {
-				/*if (pathTo.ContainsKey(leaf)) {
-					Debug.Log("OVERWRITE "+leaf.gameObject.name);
-				} else {
-					Debug.Log("Nope");
-				}*/
 				gCost[connected] = gCost[leaf] + WaypointCost(leaf, connected);
 				pathTo[connected] = leaf;
 				frontier.Add(gCost[connected]+HeuristicCostEstimate(connected, goal), connected);
@@ -80,64 +104,22 @@ function FindRoute (start : Waypoint, goal : Waypoint) : List.<Waypoint> {
 	return null;
 }
 
-function GetClosestWaypointToPoint (point : Vector3) : Waypoint {
-    var minDist : float = Mathf.Infinity;
-    var closest : Waypoint;
-    
-    var waypoints : Component[];
-    waypoints = gameObject.GetComponentsInChildren (Waypoint);
-    for (var waypoint : Component in waypoints) {
-        var currDist = (waypoint.gameObject.transform.position - point).magnitude;
-        if (currDist < minDist) {
-            minDist = currDist;
-            closest = waypoint as Waypoint;
-        }
-    }        
+//-------------------------------------------------------------------
+// Funnel Algorithm
+//-------------------------------------------------------------------
 
-    return closest;
-}
+// Returns an array of edges ordered according to a (left, right) scheme
+static function GetEdgesFromWaypointList (waypointList : List.<Waypoint>, agentUp : Vector3) : Vector3[,]	{
+	var edgeList : Vector3[,] = new Vector3[waypointList.Count-1,2]; // The array we are populating
 
-function OnDrawGizmos () {
-	if (idealWaypointPath != null) {
-		var offset : Vector3 = new Vector3(0,3,0);
-		var i : int;
-		//Draws the path between the waypoints
-		for (i = 0; i < idealWaypointPath.Count-1; i++) {
-			Gizmos.color = Color.yellow;
-			var next : Vector3 = idealWaypointPath[i+1].gameObject.transform.position;
-			Gizmos.DrawLine (idealWaypointPath[i].gameObject.transform.position + offset, next + offset);
-
-			if (i == 0) {
-				Gizmos.color = Color.green;
-			}
-			Gizmos.DrawCube(idealWaypointPath[i].gameObject.transform.position + offset, Vector3.one*0.4);
-		}
-		if (idealWaypointPath.Count > 0) {
-			Gizmos.color = Color.red;
-			Gizmos.DrawCube(idealWaypointPath[i].gameObject.transform.position + offset, Vector3.one*0.4);	
-		}
-		var tempVal : Vector3[,] = GetEdgesFromWaypointList(idealWaypointPath, Vector3.up);
-		Gizmos.color = Color.white;
-		for (i = 0; i < tempVal.length/2; i++) {
-			Gizmos.DrawCube(tempVal[i,0], Vector3.one*0.3);
-		}
-		Gizmos.color = Color.red;
-		for (i = 0; i < tempVal.length/2; i++) {
-			Gizmos.DrawCube(tempVal[i,1]+Vector3.up, Vector3.one*0.3);
-		}
-	}
-}
-
-//Returns list of vertices in order of left-right-left, etc. (edges)
-function GetEdgesFromWaypointList (waypointList : List.<Waypoint>, agentUp : Vector3) : Vector3[,]	{
-	var edgeList : Vector3[,] = new Vector3[waypointList.Count-1,2];
+	// Temporary poiners to use along the way
 	var waypointRef : Waypoint;
 	var prevWaypoint : Waypoint;
 	var navGeo : NavmeshGeometry;
 	var prevNavGeo : NavmeshGeometry;
 	var forwardVector : Vector3;
+	var edgeVerts : Vector3[];
 	var tempVert : Vector3;
-	var tempVar : Vector3[];
 	
 	for (var i : int = 1; i < waypointList.Count; i++)	{
 		waypointRef = waypointList[i];
@@ -145,10 +127,11 @@ function GetEdgesFromWaypointList (waypointList : List.<Waypoint>, agentUp : Vec
 		
 		navGeo = waypointRef.gameObject.GetComponent(NavmeshGeometry) as NavmeshGeometry;
 		prevNavGeo = prevWaypoint.gameObject.GetComponent(NavmeshGeometry) as NavmeshGeometry;
-		tempVar = sharedEdge(navGeo, prevNavGeo);
-		edgeList[i-1,0] = tempVar[0];
-		edgeList[i-1,1] = tempVar[1];
+		edgeVerts = sharedEdge(navGeo, prevNavGeo);
+		edgeList[i-1,0] = edgeVerts[0];
+		edgeList[i-1,1] = edgeVerts[1];
 		
+		// The forward-pointing vector (in the direction toward the next waypoint)
 		forwardVector = waypointRef.gameObject.transform.position - prevWaypoint.gameObject.transform.position;
 		if (AngleDir(forwardVector, edgeList[i-1,0]-edgeList[i-1,1], agentUp) == 1) {
 			tempVert = edgeList[i-1,0];
@@ -159,22 +142,21 @@ function GetEdgesFromWaypointList (waypointList : List.<Waypoint>, agentUp : Vec
 	return edgeList;
 }
 
-
-
-// Determines if the passed in triangle shares an edge with this triangle
-function sharedEdge(waypointA : NavmeshGeometry, waypointB : NavmeshGeometry) : Vector3[] {
+// Returns the coordinates of the edge shared between two NavmeshGeometry triangles
+static function sharedEdge(waypointA : NavmeshGeometry, waypointB : NavmeshGeometry) : Vector3[] {
 	var sharedVertCount : int = 0; // Counter for verts shared between the triangles.
+	var sharedVerts : Vector3[] = new Vector3[2]; // Which verts will be shared
 	var CLOSENESS : float = 0.0000000001; // If two verts are separated by this distance or less, 
 	                                             // we treat them as the same point
-	var thisVtIdx : int; var otherVtIdx : int;
+	
 	var aVerts : Vector3[] = waypointA.getTransformedVerts();
 	var bVerts : Vector3[] = waypointB.getTransformedVerts();
-	var sharedVerts : Vector3[] = new Vector3[2];
+	var aVtIdx : int; var bVtIdx : int;
 	
-	for (thisVtIdx = 0; thisVtIdx < 3; thisVtIdx++) { // For each vertex in this triangle
-		for (otherVtIdx = 0; otherVtIdx < 3; otherVtIdx++) { // For each vertex in the other triangle
-			if ((aVerts[thisVtIdx] - bVerts[otherVtIdx]).magnitude <= CLOSENESS) { // If the verts are the same i.e. on top of one another
-				sharedVerts[sharedVertCount] = aVerts[thisVtIdx];
+	for (aVtIdx = 0; aVtIdx < 3; aVtIdx++) { // For each vertex in this triangle
+		for (bVtIdx = 0; bVtIdx < 3; bVtIdx++) { // For each vertex in the other triangle
+			if ((aVerts[aVtIdx] - bVerts[bVtIdx]).magnitude <= CLOSENESS) { // If the verts are the same i.e. on top of one another
+				sharedVerts[sharedVertCount] = aVerts[aVtIdx]; // Store the current vertex
 				sharedVertCount++;
 				break; // Since a vertex is shared at most once
 			}
@@ -195,17 +177,39 @@ function FunnelAlgorithm (startPt : Vector3, goalPt : Vector3, agentUp : Vector3
 	//TODO: not finished, implement actual funnel algorithm
 }
 
-//returns -1 when to the left, 1 to the right, and 0 for forward/backward
-//adapted from: http://forum.unity3d.com/threads/31420-Left-Right-test-function
-public static function AngleDir(fwd: Vector3, targetDir: Vector3, up: Vector3) : int {
-    var perp: Vector3 = Vector3.Cross(fwd, targetDir);
-    var dir: float = Vector3.Dot(perp, up);
+//-------------------------------------------------------------------
+// Debug Visualization
+//-------------------------------------------------------------------
 
-    if (dir > 0.0) {
-        return 1;
-    } else if (dir < 0.0) {
-        return -1;
-    } else {
-        return 0;
-    }
+function OnDrawGizmos () {
+	if (idealWaypointPath != null) {
+		var offset : Vector3 = new Vector3(0,3,0);
+		var i : int;
+		//Draws the path between the waypoints
+		for (i = 0; i < idealWaypointPath.Count-1; i++) {
+			Gizmos.color = Color.yellow;
+			var next : Vector3 = idealWaypointPath[i+1].gameObject.transform.position;
+			Gizmos.DrawLine (idealWaypointPath[i].gameObject.transform.position + offset, next + offset);
+
+			if (i == 0) {
+				Gizmos.color = Color.green;
+			}
+			Gizmos.DrawCube(idealWaypointPath[i].gameObject.transform.position + offset, Vector3.one*0.4);
+		}
+		if (idealWaypointPath.Count > 0) {
+			Gizmos.color = Color.red;
+			Gizmos.DrawCube(idealWaypointPath[i].gameObject.transform.position + offset, Vector3.one*0.4);	
+		}
+
+		// Debug for edge orientation
+		var tempVal : Vector3[,] = GetEdgesFromWaypointList(idealWaypointPath, Vector3.up);
+		Gizmos.color = Color.white;
+		for (i = 0; i < tempVal.length/2; i++) {
+			Gizmos.DrawCube(tempVal[i,0], Vector3.one*0.3);
+		}
+		Gizmos.color = Color.red;
+		for (i = 0; i < tempVal.length/2; i++) {
+			Gizmos.DrawCube(tempVal[i,1], Vector3.one*0.3);
+		}
+	}
 }
