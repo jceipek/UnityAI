9b6d71eec6489e21628ac6da92f1de763f80fc5d
Alyssa <alyssa.bawgus@students.olin.edu>
2013-04-19 11:20:15 -0400

    Removed unecessary modeled, added unfinished shooting and damgage script

---
+++ BasicGame/Assets/Scripts/Die.js
@@ -0,0 +1,12 @@
+#pragma strict
+
+function Fire () {
+		var hit : RaycastHit;
+		var source : Vector3 = gameObject.transform.position;
+		var dir : Vector3 = (transform.position + Vector3.up*0.2) - source;
+		if (Physics.Raycast (source, dir, hit)) {
+				//send message to hit thing to disapear
+				gameObject.SendMessage("ApplyDamage",1.0);
+		}
+
+}
--- BasicGame/Assets/Scripts/UtilityKey.js
+++ BasicGame/Assets/Scripts/UtilityKey.js
@@ -4,14 +4,12 @@ var self : GameObject;
 var ammo : GameObject; //address
 var ammoCount : int;
 
-function Update () {
+//function Update () {
 
-	if (Input.GetKeyDown ("e")){
-		if (ammo.renderer.enabled == true){ //there is ammo visible and within a certain distance
-			ammo.renderer.enabled = false; //make ammo box disappear
-			ammoCount+=5; //add bullets	
-			}
-	}
+	//if (Input.GetKeyDown ("e")){
+	//	if (ammo.enabled == true)
+					
+	//}
 
-}
+//}
 
